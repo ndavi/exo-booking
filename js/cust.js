a7feffc4c98a571cb1d92aeb60c92387edbf431f
Clement DENIAU <cdeniau@e2r9p13.42.fr>
2017-08-19 18:22:32 +0200

    css

--- js/cust.js
+++ js/cust.js
@@ -1,36 +1,37 @@
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
+var targetOffset, currentPosition,
+    body = document.body,
+    button = document.getElementById('scrollButton'),
+    animateTime = 900;
+
+function getPageScroll() {
+  var yScroll;
+
+  if (window.pageYOffset) {
+    yScroll = window.pageYOffset;
+  } else if (document.documentElement && document.documentElement.scrollTop) {
+    yScroll = document.documentElement.scrollTop;
+  } else if (document.body) {
+    yScroll = document.body.scrollTop;
+  }
+  return yScroll;
+}
+
+button.addEventListener('click', function (event) {
+
+  targetOffset = document.getElementById(event.target.hash.substr(1)).offsetTop;
+  currentPosition = getPageScroll();
+
+  body.classList.add('in-transition');
+  body.style.WebkitTransform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
+  body.style.MozTransform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
+  body.style.transform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
+
+  window.setTimeout(function () {
+    body.classList.remove('in-transition');
+    body.style.cssText = "";
+    window.scrollTo(0, targetOffset);
+  }, animateTime);
+
+  event.preventDefault();
+
+}, false);
