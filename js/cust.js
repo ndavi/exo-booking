56197ce626918677be7a5c49352ffcedbae4fc40
Clement DENIAU <cdeniau@e2r9p13.42.fr>
2017-08-19 18:44:23 +0200

    css

--- js/cust.js
+++ js/cust.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     aLiens[i].onclick = function () {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = this.getAttribute("href").slice(1);
+        console.log(target);
         if (target.length) {
           scrollTo(document.getElementById(target).offsetTop, 1000);
           return false;
@@ -27,6 +28,7 @@ function scrollTo(element, duration) {
 }
 
 function scrollToC(element, from, to, duration) {
+  console.log('scrollToC');
   if (duration < 0) return;
   if(typeof from === "object")from=from.offsetTop;
   if(typeof to === "object")to=to.offsetTop;
@@ -34,6 +36,7 @@ function scrollToC(element, from, to, duration) {
 }
 
 function scrollToX(element, x1, x2, t, v, step, operacion) {
+  console.log('scrollToX');
   if (t < 0 || t > 1 || v <= 0) return;
   element.scrollTop = x1 - (x1-x2)*operacion(t);
   t += v * step;
@@ -43,6 +46,7 @@ function scrollToX(element, x1, x2, t, v, step, operacion) {
 }
 
 function easeOutCuaic(t){
+  console.log('easeOutCuaic');
   t--;
   return t*t*t+1;
 }
