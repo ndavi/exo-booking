b4c19dfc3ba3403216c1c3acab4a4d21070f58a5
ndavi <nico01.david@gmail.com>
2018-01-11 00:54:27 +0100

    resolve :after incorrect size after click close artist view

--- js/site.js
+++ js/site.js
@@ -147,10 +147,10 @@ function closeArtistPanel (artist) {
         artist.getElementsByClassName('desktop-excerpt')['0'].style.setProperty('display', 'block');
         artist.getElementsByClassName('expand-info')['1'].getElementsByTagName("p")[0].style.removeProperty("font-size");
     }
-    /*var youtubePlayers = artist.getElementsByClassName('youtubePlayer')
+    var youtubePlayers = artist.getElementsByClassName('youtubePlayer')
     for (var i = 0; i < youtubePlayers.length; i++) {
         youtubePlayers[i].style.setProperty('display', 'none');
-    }*/
+    }
 }
 
 function artistPanelHorizontal (artist) {
